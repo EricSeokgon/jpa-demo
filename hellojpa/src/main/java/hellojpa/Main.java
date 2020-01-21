b4f43655d3ccbc70827ded7692a4ade0edbf3810
hadesyi <leesk55@gmail.com>
2020-01-21 16:22:42 +0900

    양방향 매핑시 가장 많이 하는 실수

--- hellojpa/src/main/java/hellojpa/Main.java
+++ hellojpa/src/main/java/hellojpa/Main.java
@@ -16,26 +16,14 @@ public class Main {
         tx.begin();
         try {
 
-            //팀 저장
             Team team = new Team();
             team.setName("TeamA");
             em.persist(team);
-
-            //회원 저장
             Member member = new Member();
             member.setName("member1");
-            //member.setTeam(team); //단방향 연관관계 설정, 참조 저장
-            //역방향 (주인이 아닌 방향)만 연관관계 설정
+            //역방향(주인이 아닌 방향)만 연관관계 설정
             team.getMembers().add(member);
-
             em.persist(member);
-
-            // 새로운 팀B
-            Team teamB = new Team();
-            teamB.setName("TeamB");
-            em.persist(teamB);
-            // 회원1에 새로운 팀B 설정
-            member.setTeam(teamB);
 
             //초기화
             em.flush();
